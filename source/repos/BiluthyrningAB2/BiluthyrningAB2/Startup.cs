eda83e7171caf31b63d120d29c86740bacf98f0b
Emilia <emilia.franken@gmail.com>
2019-04-15 13:06:55 +0200

    updated according to msdn scaffolding

--- source/repos/BiluthyrningAB2/BiluthyrningAB2/Startup.cs
+++ source/repos/BiluthyrningAB2/BiluthyrningAB2/Startup.cs
@@ -63,11 +63,14 @@ namespace BiluthyrningAB2
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration config)
         {
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseAuthentication();
             app.UseMvc();
+
             app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
+            
         }
     }
 }
