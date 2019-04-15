9ff749db29d4c1dcdbbf30cfcd5199fb3c849b09
Emilia <emilia.franken@gmail.com>
2019-04-15 11:05:18 +0200

    configured startup files according to readme.txt

--- source/repos/BiluthyrningAB2/BiluthyrningAB2/Startup.cs
+++ source/repos/BiluthyrningAB2/BiluthyrningAB2/Startup.cs
@@ -57,16 +57,16 @@ namespace BiluthyrningAB2
             services.AddTransient<CarServices>();
             services.AddMvc();
 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IConfiguration config)
         {
-            
-            app.UseAuthentication();
             app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvc();
             app.UseDeveloperExceptionPage();
-
             app.UseMvcWithDefaultRoute();
         }
     }
